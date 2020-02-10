40a3c46a2ebb5c06d8dcae8132b9f0df5dcec538
Jacob Allen <jballen95173@gmail.com>
2020-02-09 22:42:19 -0500

    Added some data validation and existence checking

--- functions/evaluations/delete.js
+++ functions/evaluations/delete.js
@@ -20,24 +20,64 @@ exports.lambdaHandler = async (event, context) => {
     try {
         const requestBody = JSON.parse(event.body);
 
-        // TODO: ADD MORE DATA VALIDATION
-        // Information from the POST request needed to add a new user
+        
+        // Information from the DELETE request needed to remove an evaluation
+        if (!("EvalId" in requestBody) || requestBody.EvalId == "") {
+            response = {
+				statusCode: 400,
+				body: "This request is missing a necessary parameter - EvalId.",
+				headers: {
+					'Access-Control-Allow-Origin': '*',
+				},
+			}
+			return response;
+        }
         const evalId = requestBody.EvalId;
+
+        if (!("CompetencyId_Timestamp" in requestBody) || requestBody.CompetencyId_Timestamp == "") {
+            response = {
+				statusCode: 400,
+				body: "This request is missing a necessary parameter - CompetencyId_Timestamp.",
+				headers: {
+					'Access-Control-Allow-Origin': '*',
+				},
+			}
+			return response;
+        }
         const compId_timestamp = requestBody.CompetencyId_Timestamp;
 
+        //The key representing the parameters for the delete and get request
+        //Note that AWS documentation will often tell you to format them like attrName: {"S": value}
+        //but this does not actually work, you need to just do it like this
         key = {
             "UserIdBeingEvaluated": evalId,
             "CompetencyId_Timestamp": compId_timestamp
             
         }
 
-        // Put the user in the database
-        await deleteEval(key);
+        // Check if an evaluation with the given parameters is in the database
+        const getResponse = await getEval(key);
+        // console.log(JSON.stringify(getResponse));
+        // Log the JSON object that was found in the database
+        console.log(getResponse);
+        //If the response didn't have an item in it (nothing was found in the database), return a 404 (not found)
+        if (!("Item" in getResponse)) {
+            response = {
+                statusCode: 404,
+                body: "An evaluation was not found with the given parameters",
+                headers: {
+                    'Access-Control-Allow-Origin': '*',
+                },
+            }
+            return response;
+        }
+        //Delete the evaluation from the database (at this point we know it's there)
+        const delResponse = await deleteEval(key);
         
-        // Generate the response for a successful post
+        // Generate the response for a successful post, include the JSON from the get request
         response = {
             statusCode: 200,
-            body: "Delete Successful",
+            body: "Delete Successful: " + JSON.stringify(getResponse),
             headers: {
                 'Access-Control-Allow-Origin': '*',
             },
@@ -51,13 +91,29 @@ exports.lambdaHandler = async (event, context) => {
 };
 
 /**
+ * Deletes the item with the given key from the table
  
- * @param {String} key - JSON object representing the key of the entry to remove
+ * @param {Object} key - JSON object representing the key of the entry to remove
  * 
- * @returns {Object} object - a promise representing this put request
+ * @returns {Object} object - a promise representing this delete request
  */
 function deleteEval(key) {
     return ddb.delete({
+        TableName: EVALUATIONS_DDB_TABLE_NAME,
+        Key: key
+    }).promise();
+}
+
+/**
+ * Gets the item with the given key from the evals table, this will be used as a check
+ * for whether or not the item is actually in the table
+ * 
+ * @param {Object} key - JSON object representing the key of the entry to get
+ * 
+ * @returns {Object} object - a promise representing this get request
+ */
+function getEval(key) {
+    return ddb.get({
         TableName: EVALUATIONS_DDB_TABLE_NAME,
         Key: key
     }).promise();
