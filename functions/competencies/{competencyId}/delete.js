92dd26915b9a308642c2d0bbc9ec501ce74c0099
destinideinde <40705170+destinideinde@users.noreply.github.com>
2020-02-12 17:12:59 -0500

    Deletes with a path parameter and checks that data exists

--- functions/competencies/{competencyId}/delete.js
+++ functions/competencies/{competencyId}/delete.js
@@ -18,9 +18,9 @@ const COMPETENCIES_DDB_TABLE_NAME = process.env.COMPETENCIES_DDB_TABLE_NAME; // 
  */
 exports.lambdaHandler = async (event, context) => {
     try {
-        const requestBody = JSON.parse(event.body);
-        const competencyId = requestBody.CompetencyId;
-		if (!("CompetencyId" in requestBody) || competencyId == "") {
+        const competencyId = event.pathParameters.competencyId;
+
+		if (isEmptyObject(competencyId)) {
 			response = {
 				statusCode: 400,
 				body: "This request is missing a necessary parameter - CompetencyId.",
@@ -29,10 +29,10 @@ exports.lambdaHandler = async (event, context) => {
 				},
 			}
 			return response;
-		} else if (!/^\d+$/.test(competencyId)) {
+         } else if (!/^\d+$/.test(competencyId)) {
 			response = {
 				statusCode: 400,
-				body: "This request must contain a non-empty CompetencyId with only numeric characters. You entered : " + competencyId,
+				body: "This request must contain a non-empty CompetencyId with only numeric characters. You entered : " + JSON.stringify(competencyId),
 				headers: {
 					'Access-Control-Allow-Origin': '*',
 				},
@@ -40,14 +40,31 @@ exports.lambdaHandler = async (event, context) => {
 			return response;
 		}
 
+        // Check if an evaluation with the given parameters is in the database
+        const getResponse = await getCompetency(competencyId);
+
+       
+        //If the response didn't have an item in it (nothing was found in the database), return a 404 (not found)
+        if (!("Item" in getResponse)) {
+            response = {
+                statusCode: 404,
+                body: "A comptetency was not found with the given id  - " + competencyId,
+                headers: {
+                    'Access-Control-Allow-Origin': '*',
+                },
+            }
+            return response;
+        }
+
+
         // Remove a competency from the database
         await removeCompetency(competencyId);
 
         // TODO : It still returns success when the id was valid (a number) but did not actually exists in the database (ie nothing happened)
         // Generate the response for a successful delete
         response = {
-            statusCode: 201,
-            body: "The comptetency with the following id has been deleted - " + JSON.stringify(competencyId),
+            statusCode: 204,
+            body: "The comptetency with the following id has been deleted - " + competencyId,
             headers: {
                 'Access-Control-Allow-Origin': '*',
             },
@@ -72,4 +89,33 @@ function removeCompetency(competencyId) {
             "CompetencyId" : competencyId,
         }
     }).promise();
+}
+
+/**
+ * @param {Object} competencyId - the competencyId key that will be removed from the database
+ * 
+ * @returns {Object} object - a promise representing this delete request
+ */
+function getCompetency(competencyId) {
+    return ddb.get({
+        TableName: COMPETENCIES_DDB_TABLE_NAME,
+        Key: {
+            "CompetencyId" : competencyId,
+        }
+    }).promise();
+}
+
+/**
+ * Checks if the provided JSON object is empty {} or not
+ * @param {JSON} obj - The object to check for emptiness
+ * 
+ * @returns {boolean} True if this JSON object is empty, false if it is not empty
+ */
+function isEmptyObject(obj) {
+    for (var key in obj) {
+        if (Object.prototype.hasOwnProperty.call(obj, key)) {
+        return false;
+        }
+    }
+    return true;
 }
