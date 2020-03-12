71b5416d0b70174c9b6c4a9b2cbc5504d956a27f
Jacob Allen <jballen95173@gmail.com>
2020-03-11 21:14:51 -0400

    Moving authentication logic to layer

--- functions/evaluations/post.js
+++ functions/evaluations/post.js
@@ -1,5 +1,6 @@
 let response;
 
+const auth = require('/opt/auth');
 const AWS = require('aws-sdk');
 const ddb = new AWS.DynamoDB.DocumentClient();
 const EVALUATIONS_DDB_TABLE_NAME = process.env.EVALUATIONS_DDB_TABLE_NAME; // Allows us to access the environment variables defined in the Cloudformation template
@@ -24,11 +25,11 @@ const validRoles = ["Admin", "Faculty/Staff", "Coach", "Mentor"];
 exports.lambdaHandler = async (event, context) => {
     try {
         console.log(event.requestContext);
-        let indicator = verifyAuthorizerExistence(event);
+        let indicator = auth.verifyAuthorizerExistence(event);
         if (indicator != null) {
             return indicator;
         }
-        indicator = verifyValidRole(event, validRoles);
+        indicator = auth.verifyValidRole(event, validRoles);
         if (indicator != null) {
             return indicator;
         }
@@ -244,48 +245,48 @@ function addEval(eval) {
     }).promise();
 }
 
-/**
- * Verifies that the event contains an authorizer with some claims
- * @param {Object} event the event object from the lambda
- * @returns the error response that should be returned if the authorizer is incorrect,
- * else returns null to indicate success
- */
-function verifyAuthorizerExistence(event) {
-    if (!("authorizer" in event.requestContext) || !("claims" in event.requestContext.authorizer)) {
-        response = {
-            statusCode: 401,
-            body: "Authorization is missing from request",
-            headers: {
-                'Access-Control-Allow-Origin': '*',
-            },
-        }
-        return response;
-    }
-    return null;
-}
+// /**
+//  * Verifies that the event contains an authorizer with some claims
+//  * @param {Object} event the event object from the lambda
+//  * @returns the error response that should be returned if the authorizer is incorrect,
+//  * else returns null to indicate success
+//  */
+// function verifyAuthorizerExistence(event) {
+//     if (!("authorizer" in event.requestContext) || !("claims" in event.requestContext.authorizer)) {
+//         response = {
+//             statusCode: 401,
+//             body: "Authorization is missing from request",
+//             headers: {
+//                 'Access-Control-Allow-Origin': '*',
+//             },
+//         }
+//         return response;
+//     }
+//     return null;
+// }
 
-function verifyValidRole(event, validRoles) {
-    if (!("custom:role" in event.requestContext.authorizer.claims)) {
-        response = {
-            statusCode: 403,
-            body: "User does not have any assigned role",
-            headers: {
-                'Access-Control-Allow-Origin': '*',
-            },
-        }
-        return response;
-    }
-    const role = event.requestContext.authorizer.claims['custom:role'];
-    if (!validRoles.includes(role)) {
-        response = {
-            statusCode: 403,
-            body: "User role is not permitted to perform this action. Role " + role 
-            + " must be one of " + validRoles.toString(),
-            headers: {
-                'Access-Control-Allow-Origin': '*',
-            },
-        }
-        return response;
-    }
-    return null;
-}
+// function verifyValidRole(event, validRoles) {
+//     if (!("custom:role" in event.requestContext.authorizer.claims)) {
+//         response = {
+//             statusCode: 403,
+//             body: "User does not have any assigned role",
+//             headers: {
+//                 'Access-Control-Allow-Origin': '*',
+//             },
+//         }
+//         return response;
+//     }
+//     const role = event.requestContext.authorizer.claims['custom:role'];
+//     if (!validRoles.includes(role)) {
+//         response = {
+//             statusCode: 403,
+//             body: "User role is not permitted to perform this action. Role " + role 
+//             + " must be one of " + validRoles.toString(),
+//             headers: {
+//                 'Access-Control-Allow-Origin': '*',
+//             },
+//         }
+//         return response;
+//     }
+//     return null;
+// }
